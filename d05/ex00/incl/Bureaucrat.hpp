45a1d3e73848e70f10ba7565e65b7e58db969c51
Richard HOHLS <rhohls@c4r5s10.wethinkcode.co.za>
2019-06-12 07:47:32 +0200

    final d05

--- d05/ex00/incl/Bureaucrat.hpp
+++ d05/ex00/incl/Bureaucrat.hpp
@@ -18,14 +18,14 @@
 
 
 class Bureaucrat {
+	
+	public:
 	class GradeTooHighException : public std::exception {
 		const char * what () const throw ();
 	};
 	class GradeTooLowException : public std::exception {
 		const char * what () const throw ();
 	};
-	
-	public:
 		Bureaucrat(std::string input_name, int input_grade);
 		
 		Bureaucrat(Bureaucrat const & src);
